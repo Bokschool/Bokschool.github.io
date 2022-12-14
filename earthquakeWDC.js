ab952305679b26b6f9cb5081bf89d1e20d003c2d
Bok <bokyann.chou@prismplus.sg>
2022-12-14 17:58:45 +0800

    commit

--- earthquakeWDC.js
+++ earthquakeWDC.js
@@ -2,7 +2,6 @@
     var myConnector = tableau.makeConnector();
     myConnector.getSchema = function (schemaCallback) {
         var cols = [
-            {id: "id", dataType: tableau.dataTypeEnum.string},
             {id: "BIRTH_YEAR", dataType: tableau.dataTypeEnum.string}, 
             {id: "GENDER", dataType: tableau.dataTypeEnum.string},
             {id: "ETHNICITY", dataType: tableau.dataTypeEnum.string},
@@ -13,18 +12,17 @@
         var tableSchema = {
             id: "IncrementalRefresh",
             alias: "MaintTable",
-            columns: cols,
-            incrementalColumnId: "id"
+            columns: cols
         };
         schemaCallback([tableSchema]);
     };
     myConnector.getData = function (table, doneCallback) {
         $.getJSON("https://data.cityofnewyork.us/api/views/25th-nujf/rows.json", function(resp) {
-        var lastId = parseInt(table.incrementValue || -1);
+        var feat = resp.data,
+            tableData = [];
         // Iterate over the JSON object
         for (var i = 0, len = feat.length; i < len; i++) {
             tableData.push({
-                "id": feat[i][0],
                 "BIRTH_YEAR": feat[i][8],
                 "GENDER": feat[i][9],
                 "ETHNICITY": feat[i][10],
@@ -40,7 +38,7 @@
     tableau.registerConnector(myConnector);
     $(document).ready(function () {
         $("#submitButton").click(function () {
-            tableau.connectionName = "Test ";
+            tableau.connectionName = "Test";
             tableau.submit();
         });
     });
